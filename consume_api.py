cca0df5e9f125176092bbaa889eba3dd8d82c11b
BalbinoEsen <balbino.aylagas@esen.edu.sv>
2021-05-24 19:46:10 -0600

    modify consume_api, video logic, video resource

--- consume_api.py
+++ consume_api.py
@@ -1,7 +1,12 @@
 import requests
-from core.pyba_database import PybaDatabase
 
-response = requests.get("http://127.0.0.1:5000/video/0")
+data = {
+    "name": "shark",
+    "views": 40,
+    "likes": 50,
+}
+
+response = requests.put("http://127.0.0.1:5000/video/3", data=data)
 print(response)
 messageJson = response.json()
 print(messageJson)
--- resource/video.py
+++ resource/video.py
@@ -1,12 +1,30 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+from logic.video_logic import VideoLogic
 
 
 class Video(Resource):
+    def __init__(self):
+        self.video_put_args = self.createParser()
+
+    def createParser(self):
+        args = reqparse.RequestParser()
+        args.add_argument("name", type=str, help="name of the video")
+        args.add_argument("views", type=int, help="views of the video")
+        args.add_argument("likes", type=int, help="likes of the video")
+        return args
+
     def get(self, id):
-        return {"data": id}, 200
+        logic = VideoLogic()
+        result = logic.getVideoById(id)
+        if len(result) == 0:
+            return {}
+        return result[0], 200
 
     def put(self, id):
-        return {"data": id}, 200
+        video = self.video_put_args.parse_args()
+        logic = VideoLogic()
+        rows = logic.insertVideo(video)
+        return {"rowsAffected": rows}, 200
 
     def patch(self, id):
         return {"data": id}, 200
